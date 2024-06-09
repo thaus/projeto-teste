92d988045db42d1fc87039313241c4f58c602a23
mathausr <reselane3@hotmail.com>
2024-06-09 19:17:27 -0300

    change unit test of movie list

--- src/app/features/movies-list/movies-list.component.spec.ts
+++ src/app/features/movies-list/movies-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MoviesListComponent } from './movies-list.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('MoviesListComponent', () => {
   let component: MoviesListComponent;
@@ -8,9 +10,9 @@ describe('MoviesListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MoviesListComponent]
-    })
-    .compileComponents();
+      imports: [MoviesListComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MoviesListComponent);
     component = fixture.componentInstance;
