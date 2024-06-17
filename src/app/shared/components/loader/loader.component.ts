ce72dc40dfb3eced74d8a390dfdf01d36aa18f53
mathausr <reselane3@hotmail.com>
2024-06-17 17:47:01 -0300

    add loader component

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  standalone: true,
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
+  imports: [],
+})
+export class LoaderComponent {}
