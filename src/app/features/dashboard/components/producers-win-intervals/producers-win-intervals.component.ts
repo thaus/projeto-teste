225323c2372e19123ee7bcfaba3addcdece82827
mathausr <reselane3@hotmail.com>
2024-06-17 17:48:59 -0300

    reuse component list in producers component

--- src/app/features/dashboard/components/producers-win-intervals/producers-win-intervals.component.ts
+++ src/app/features/dashboard/components/producers-win-intervals/producers-win-intervals.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Producers } from '../../../../shared/models/producers.model';
+import { ProducersListComponent } from './producers-list/producers-list.component';
 
 @Component({
   selector: 'app-producers-win-intervals',
   standalone: true,
   templateUrl: './producers-win-intervals.component.html',
   styleUrl: './producers-win-intervals.component.scss',
-  imports: [CommonModule],
+  imports: [CommonModule, ProducersListComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProducersWinIntervalsComponent {
